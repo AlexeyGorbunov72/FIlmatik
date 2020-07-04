e8d9946253ac906d2a42db93ea268be4a2306bf3
AnyDodic <korri.13@inbox.ru>
2020-07-04 07:33:34 +0300

    fix bug in confetti pod

---
+++ FIlmatik/ViewControllers Group/AnimatedTabBarController.swift
@@ -0,0 +1,35 @@
+//
+//  AnimatedTabBarController.swift
+//  FIlmatik
+//
+//  Created by Alexey on 04.07.2020.
+//  Copyright © 2020 Alexey. All rights reserved.
+//
+
+import UIKit
+
+class AnimatedTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    private var bounceAnimation: CAKeyframeAnimation = {
+        let bounceAnimation = CAKeyframeAnimation(keyPath: "transform.scale")
+        bounceAnimation.values = [1.0, 1.4, 0.9, 1.02, 1.0]
+        bounceAnimation.duration = TimeInterval(0.3)
+        bounceAnimation.calculationMode = CAAnimationCalculationMode.cubic
+        return bounceAnimation
+    }()
+    override func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
+            // find index if the selected tab bar item, then find the corresponding view and get its image, the view position is offset by 1 because the first item is the background (at least in this case)
+         guard let idx = tabBar.items?.index(of: item), tabBar.subviews.count > idx + 1, let imageView = tabBar.subviews[idx + 1].subviews.flatMap { $0 as? UIImageView }.first else {
+                   return
+               }
+
+               imageView.layer.add(bounceAnimation, forKey: nil)
+           
+        }
+    
+
+}
