95e3f909241db35d6e1a983ed137a81d85210fc2
AnyDodic <korri.13@inbox.ru>
2020-07-04 06:41:15 +0300

    add simple navigations

---
+++ FIlmatik/ViewControllers Group/FeedViewController.swift
@@ -0,0 +1,23 @@
+//
+//  FeedViewController.swift
+//  FIlmatik
+//
+//  Created by Alexey on 04.07.2020.
+//  Copyright © 2020 Alexey. All rights reserved.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+    }
+    func setupView(){
+        title = "Рекомендации"
+    } 
+
+    
+
+}
